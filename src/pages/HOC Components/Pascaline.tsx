abed944d0b2437b39fa006e7fc694bd38b11805b
ElmikoTheNiceFella <akprogamer23@gmail.com>
2023-09-16 19:24:18 +0300

    Removed Console Logs

--- src/pages/HOC Components/Pascaline.tsx
+++ src/pages/HOC Components/Pascaline.tsx
@@ -13,20 +13,16 @@ function Pascaline({ nextFunc, prevFunc }: PropType) {
 
   const handlePlus = (index: number) => {
     if (pascalineArray[index] < 9) {
-      console.log(knobRotation);
       setKnobRotation((prev) => {
         const arr = [...prev];
         arr[index] += 72;
         return arr;
       });
-      console.log(knobRotation);
-      console.log(pascalineArray);
       setPascalineArray((prev) => {
         const arr = [...prev];
         arr[index]++;
         return arr;
       });
-      console.log(pascalineArray);
     }
   };
 
