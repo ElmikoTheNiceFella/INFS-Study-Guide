b6271e9152136e3581f4fb9442d2fd31a9fd9d75
ElmikoTheNiceFella <akprogamer23@gmail.com>
2023-10-12 15:26:04 +0300

    Getting Grades

--- src/pages/GradeCalc.tsx
+++ src/pages/GradeCalc.tsx
@@ -7,8 +7,9 @@ type Grade = {
   midterm:number[];
 }
 
-function GradeCalc() {
+type Grades = "quiz" | "labs" | "assignment" | "midterm";
 
+function GradeCalc() {
   const [grades, setGrades] = useState<Grade>({
     quiz: [0,0],
     labs: [0,0,0,0,0,0,0,0],
@@ -18,21 +19,45 @@ function GradeCalc() {
 
   const [numberOfInputs, setNumberOfInputs] = useState<number[]>([0,0,0,0]);
 
-  const handleAddGrade = (key:string, index:number) => {
-    
+  const [quiz, setQuiz] = useState(0);
+  const [labs, setLabs] = useState(0);
+  const [assignment, setAssignment] = useState(0);
+  const [midterm, setMidterm] = useState(0);
+
+  const handleAddGrade = (key:Grades, index:number) => {
+    setGrades(() => {
+      let temp = {...grades};
+      temp[key][numberOfInputs[index]] = index == 0 ? quiz :
+                                         index == 1 ? labs :
+                                         index == 2 ? assignment :
+                                                       midterm;
+      return temp;
+    })
+    setNumberOfInputs((prev) => {
+      let temp = prev;
+      temp[index]++;
+      return temp;
+    });
   }
+
+  const gradesKeys = Object.keys(grades) as Grades[];
 
   return (
     <div className="flex flex-col items-center h-screen justify-center">
       <h1 className="text-4xl font-semibold text-center w-[60%] leading-[3rem] mb-12">
         Calculate you're grade
       </h1>
-      {Object.keys(grades).map((x, i) => (
+      {gradesKeys.map((x, i) => (
         <div className="mt-4 space-y-4">
           <p className="text-2xl font-semibold">
             {x[0].toUpperCase() + x.substring(1)} Grades:
           </p>
           <input
+            onChange={(e) => 
+              i == 0 ? setQuiz(+e.target.value) :
+              i == 1 ? setLabs(+e.target.value) :
+              i == 2 ? setAssignment(+e.target.value) 
+                     : setMidterm(+e.target.value)}
             className="border-2 border-[#202020] h-12 w-[200px] text-center text-2xl mr-4"
             type="number"
           />
@@ -41,7 +66,15 @@ function GradeCalc() {
           </button>
         </div>
       ))}
-      <button className="bg-[#2B78E4] w-[416px] h-[65px] text-2xl text-white mt-12">
+      <button onClick={() => {
+        console.log(grades)
+        setGrades({
+          quiz: [0, 0],
+          labs: [0, 0, 0, 0, 0, 0, 0, 0],
+          assignment: [0],
+          midterm: [0],
+        })
+      }} className="bg-[#2B78E4] w-[416px] h-[65px] text-2xl text-white mt-12">
         CALCULATE
       </button>
     </div>
