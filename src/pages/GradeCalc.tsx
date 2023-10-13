c99ed1026c2bf8e7842b64b3951f5ef2c2c130c3
Abd El Hakim Akhadkhou <akprogamer23@gmail.com>
2023-10-13 15:42:50 +0300

    man

--- src/pages/GradeCalc.tsx
+++ src/pages/GradeCalc.tsx
@@ -46,7 +46,54 @@ function GradeCalc() {
     console.log(grades);
     console.log(numberOfInputs);
 
-    // Calculate final grade here
+    let finalGrade = 0;
+    let denominator = 0;
+
+    gradesKeys.forEach(key => {
+      let weight = 0;
+      let index = 0;
+      let max = 0;
+
+      switch(key) {
+        case "quiz":
+          weight = 5;
+          index = 0;
+          max = 5;
+          break;
+        case "assignment":
+          weight = 1.5;
+          index = 2;
+          max = 36;
+          break;
+        case "labs":
+          weight = 1.25;
+          index = 1;
+          max = 100;
+          break;
+        case "midterm":
+          weight = 20
+          index = 3;
+          max = 30;
+          break;
+        default:
+          weight = 0;
+          break;
+      }
+
+      let total = weight * numberOfInputs[index];
+
+      let userGrade =
+        ((grades[key].reduce((acc, curr) => acc + curr, 0) /
+          numberOfInputs[index]) / max) * total;
+      
+      finalGrade += userGrade;
+      denominator += total;
+
+      console.log(key,userGrade,numberOfInputs[index],total)
+      
+    })
+
+    console.log((finalGrade/denominator) * 100);
 
     setNumberOfInputs([0, 0, 0, 0]);
     setGrades({
@@ -63,25 +110,40 @@ function GradeCalc() {
         Calculate you're grade
       </h1>
       {gradesKeys.map((x, i) => (
-        <div className="mt-4 space-y-4">
+        <div key={"grade-"+x} className="mt-4">
           <p className="text-2xl font-semibold">
             {x[0].toUpperCase() + x.substring(1)} Grades:
           </p>
-          <input
-            onChange={(e) => 
-              i == 0 ? setQuiz(+e.target.value) :
-              i == 1 ? setLabs(+e.target.value) :
-              i == 2 ? setAssignment(+e.target.value) 
-                     : setMidterm(+e.target.value)}
-            className="border-2 border-[#202020] h-12 w-[200px] text-center text-2xl mr-4"
-            type="number"
-          />
-          <button onClick={() => handleAddGrade(x, i)} className="bg-[#2B78E4] w-[200px] h-12 text-xl text-white mt-12">
-            Add Grade
-          </button>
+          <div className="flex items-center">
+            <span className="py-2 px-6 text-white font-semibold text-2xl bg-[#2B78E4] absolute -ml-20">
+              {numberOfInputs[i]}
+            </span>
+            <input
+              onChange={(e) =>
+                i == 0
+                  ? setQuiz(+e.target.value)
+                  : i == 1
+                  ? setLabs(+e.target.value)
+                  : i == 2
+                  ? setAssignment(+e.target.value)
+                  : setMidterm(+e.target.value)
+              }
+              className="border-2 border-[#202020] h-12 w-[200px] text-center text-2xl mr-4"
+              type="number"
+            />
+            <button
+              onClick={() => handleAddGrade(x, i)}
+              className="bg-[#2B78E4] w-[200px] h-12 text-xl text-white"
+            >
+              Add Grade
+            </button>
+          </div>
         </div>
       ))}
-      <button onClick={handleCalculate} className="bg-[#2B78E4] w-[416px] h-[65px] text-2xl text-white mt-12">
+      <button
+        onClick={handleCalculate}
+        className="bg-[#2B78E4] w-[416px] h-[65px] text-2xl text-white mt-12"
+      >
         CALCULATE
       </button>
     </div>
