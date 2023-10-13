beeb2e54d7b3a8db36683177b111fc78cf97ce06
Abd El Hakim Akhadkhou <akprogamer23@gmail.com>
2023-10-13 16:49:22 +0300

    Grade calculator done

--- src/pages/GradeCalc.tsx
+++ src/pages/GradeCalc.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "../App.css"
 
 type Grade = {
   quiz:number[];
@@ -24,13 +25,15 @@ function GradeCalc() {
   const [assignment, setAssignment] = useState(0);
   const [midterm, setMidterm] = useState(0);
 
+  const [result, setResult] = useState(0);
+
   const handleAddGrade = (key:Grades, index:number) => {
     setGrades(() => {
-      let temp = {...grades};
+      let temp = { ...grades };
       temp[key][numberOfInputs[index]] = index == 0 ? quiz :
                                          index == 1 ? labs :
                                          index == 2 ? assignment :
-                                                       midterm;
+                                                      midterm;
       return temp;
     })
     setNumberOfInputs((prev) => {
@@ -40,11 +43,22 @@ function GradeCalc() {
     });
   }
 
+  const handleRemoveGrade = (key: Grades, index: number) => {
+    setGrades(() => {
+      let temp = { ...grades };
+      temp[key][numberOfInputs[index]-1] = 0;
+      return temp;
+    });
+    setNumberOfInputs((prev) => {
+      let temp = prev;
+      temp[index]--;
+      return temp;
+    });
+  };
+
   const gradesKeys = Object.keys(grades) as Grades[];
 
   const handleCalculate = () => {
-    console.log(grades);
-    console.log(numberOfInputs);
 
     let finalGrade = 0;
     let denominator = 0;
@@ -58,7 +72,7 @@ function GradeCalc() {
         case "quiz":
           weight = 5;
           index = 0;
-          max = 5;
+          max = 10;
           break;
         case "assignment":
           weight = 1.5;
@@ -88,12 +102,12 @@ function GradeCalc() {
       
       finalGrade += userGrade;
       denominator += total;
-
-      console.log(key,userGrade,numberOfInputs[index],total)
       
     })
 
-    console.log((finalGrade/denominator) * 100);
+    let finalResult = Math.round((finalGrade / denominator) * 10000);
+
+    setResult(finalResult/100);
 
     setNumberOfInputs([0, 0, 0, 0]);
     setGrades({
@@ -102,15 +116,30 @@ function GradeCalc() {
       assignment: [0],
       midterm: [0],
     });
+    setQuiz(0);
+    setAssignment(0);
+    setLabs(0);
+    setMidterm(0);
   }
 
   return (
     <div className="flex flex-col items-center h-screen justify-center">
+      <div
+        style={{
+          display: result ? "grid" : "none",
+        }}
+        onClick={() => setResult(0)}
+        className="h-screen bg-[#00000050] place-content-center w-full absolute"
+      >
+        <div className="w-[300px] h-[150px] text-2xl font-semibold grid place-content-center bg-white">
+          Your Grade Is: {result}%
+        </div>
+      </div>
       <h1 className="text-4xl font-semibold text-center w-[60%] leading-[3rem] mb-12">
         Calculate you're grade
       </h1>
       {gradesKeys.map((x, i) => (
-        <div key={"grade-"+x} className="mt-4">
+        <div key={"grade-" + x} className="mt-4">
           <p className="text-2xl font-semibold">
             {x[0].toUpperCase() + x.substring(1)} Grades:
           </p>
@@ -128,14 +157,23 @@ function GradeCalc() {
                   ? setAssignment(+e.target.value)
                   : setMidterm(+e.target.value)
               }
-              className="border-2 border-[#202020] h-12 w-[200px] text-center text-2xl mr-4"
+              value={
+                i == 0 ? quiz : i == 1 ? labs : i == 2 ? assignment : midterm
+              }
+              className="border-2 number-input border-[#202020] h-12 w-[200px] text-center text-2xl mr-4"
               type="number"
             />
             <button
               onClick={() => handleAddGrade(x, i)}
-              className="bg-[#2B78E4] w-[200px] h-12 text-xl text-white"
+              className="bg-[#2B78E4] w-[100px] h-12 text-xl text-white"
             >
-              Add Grade
+              Add
+            </button>
+            <button
+              onClick={() => handleRemoveGrade(x, i)}
+              className="bg-[#2B78E4] w-[130px] h-12 text-xl text-white ml-4"
+            >
+              Remove
             </button>
           </div>
         </div>
